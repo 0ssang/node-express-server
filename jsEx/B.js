a45416b9b4de9c5fc53f6cea30d36b4f6cdfbe5f
조영상 <51320595+0ssang@users.noreply.github.com>
2024-07-18 23:19:31 +0900

    module 개념 학습

---
+++ jsEx/B.js
@@ -0,0 +1,14 @@
+const A = require('./A');
+
+console.log(A + ' in B.js');
+
+// * JS는 순환참조를 허용한다. 
+/**
+ * 자주 사용하는 기본(Node.js 내장) 모듈
+ * global : 전역 객체
+ * process : vmfhtptm rorcp
+ * OS : 운영체제 모듈
+ * fs : 파일 시스템 모듈
+ * util : 유틸 모듈
+ * url : URI 모듈
+ */
