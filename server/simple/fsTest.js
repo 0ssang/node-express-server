86685097d5f37d2f90edd51e98ec4edc52e9be87
조영상 <51320595+0ssang@users.noreply.github.com>
2024-07-19 00:11:54 +0900

    파일 경로 확인 & 절대 경로 사용

--- server/simple/fsTest.js
+++ server/simple/fsTest.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const fs = require('fs').promises;
+const path = require('path');
+
+// *** 현재 작업 디렉토리를 콘솔에 출력 ***
+console.log('Current working directory:', process.cwd());
 
 http.createServer(async (req, res) => {
     try{
-        const f = await fs.readFile('./fs_test.html');
+        const filePath = path.join(__dirname, 'fs_test.html');
+        const f = await fs.readFile(filePath);
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         // 200 이면 요청 성공
         res.end(f); // 요청 종료
@@ -16,4 +21,4 @@ http.createServer(async (req, res) => {
 })
     .listen(8080, () => {
         console.log('8080포트에서 서버 연결 중...');
-    })
+    });
