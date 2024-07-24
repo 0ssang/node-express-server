c3d9d2e7aad7601eb3bd351c9b244cedef2ff922
조영상 <51320595+0ssang@users.noreply.github.com>
2024-07-24 21:54:35 +0900

    redis: 캐싱 적용

--- server/express-ex/airkorea-axios.js
+++ server/express-ex/airkorea-axios.js
@@ -36,11 +36,27 @@ const dotenv = require('dotenv');
 dotenv.config({ path: path.join(process.cwd(), "/.env") });
 const morgan = require('morgan');
 const axios = require('axios');
+
+/* express app generate */
 const express = require('express');
 const app = express();
 
+/* redis connect */
+const redis = require('redis');
+const client = redis.createClient({
+    url: 'redis://127.0.0.1:6379'
+});
+let clientConnected = false;
+client.on('error', (err) => {
+    console.log('Redis Error : ' + err);
+});
+client.on('ready', () => {
+    clientConnected = true;
+    console.log('Redis client connected');
+});
+
 /* 포트 설정 */
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT);
 
 /* 공통 미들웨어 */
 app.use(morgan('dev'));
@@ -49,26 +65,66 @@ app.use(express.urlencoded({ extended: true }));
 
 /* 라우팅 설정 */
 app.get('/airkorea', async (req, res) => {
-    const serviceKey = process.env.airServiceKey;
-    const airUrl = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?";
+    await client.lRange('airItems', 0, -1, async (err, cachedItems) => {
+        if(err) throw err;
+        if(cachedItems.length){
+            res.send(`데이터가 캐시에 있습니다. <br>
+                관측 지역: ${cachedItems[0]} / 관측 시간: ${cachedItems[1]} <br>
+                미세먼지: ${cachedItems[2]} 초미세먼지: ${cachedItems[3]} 입니다.`);
+        } else {
+            const serviceKey = process.env.airServiceKey;
+            const airUrl = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?";
+            let params = encodeURI('serviceKey') + '=' + serviceKey;
+            params += '&' + encodeURI('numOfRows') + '=' + encodeURI('1');
+            params += '&' + encodeURI('pageNo') + '=' + encodeURI('1');
+            params += '&' + encodeURI('dataTerm') + '=' + encodeURI('DAILY');
+            params += '&' + encodeURI('ver') + '=' + encodeURI('1.3');
+            params += '&' + encodeURI('stationName') + '=' + encodeURI('마포구');
+            params += '&' + encodeURI('returnType') + '=' + encodeURI('json');
 
-    let params = encodeURI('serviceKey') + '=' + serviceKey;
-    params += '&' + encodeURI('numOfRows') + '=' + encodeURI('1');
-    params += '&' + encodeURI('pageNo') + '=' + encodeURI('1');
-    params += '&' + encodeURI('dataTerm') + '=' + encodeURI('DAILY');
-    params += '&' + encodeURI('ver') + '=' + encodeURI('1.3');
-    params += '&' + encodeURI('stationName') + '=' + encodeURI('강서구');
-    params += '&' + encodeURI('returnType') + '=' + encodeURI('json');
+            const url = airUrl + params;
 
-    const url = airUrl + params;
+            try{
+                const result = await axios.get(url);
+                // ** axios로 받은 결과는 .data를 붙여줘야함
+                
+                const airItem = {
+                    "location": "마포구",
+                    "time": result.data.response.body.items[0]["dataTime"],
+                    "pm10": result.data.response.body.items[0]["pm10Value"],
+                    "pm25": result.data.response.body.items[0]["pm25Value"]
+                };
+                const badAir = [];
+        
+                if(airItem.pm10 <= 30) {
+                    badAir.push("좋음👍👍");
+                }else if(airItem.pm10 > 30 && airItem.pm10 <= 80){
+                    badAir.push("보통😐😐");
+                }else {
+                    badAir.push("나쁨☹️☹️");
+                }
+                if(airItem.pm25 <= 15){
+                    badAir.push("좋음");
+                }else if(airItem.pm25 > 15 && airItem.pm25 <= 35){
+                    badAir.push("보통");
+                }else {
+                    badAir.push("나쁨");
+                }
+                
+                const airItems = [airItem.location, airItem.time, badAir[0], badAir[1]];
+                airItems.forEach((val) => {
+                    // Redis 저장
+                    client.rPush('airItems', val);
+                });
+                client.expire('airItems', 60 * 60);
 
-    try{
-        const result = await axios.get(url);
-        // ** axios로 받은 결과는 .data를 붙여줘야함
-        res.json(result.data);
-    } catch (error) {
-        console.log(error);
-    }
+                res.send('캐시된 데이터가 없습니다.');
+            } catch (error) {
+                console.error('Error fetching data from API:', error);
+                res.status(500).send('Internal Server Error');
+            }
+        }
+    })
 });
 
 /* 서버와 포트 연결 */
