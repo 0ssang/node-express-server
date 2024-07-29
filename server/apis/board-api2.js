e35de0a8bede2908b09612c47ad13a696b3f4594
조영상 <51320595+0ssang@users.noreply.github.com>
2024-07-29 22:17:29 +0900

    cors 모듈 적용

--- server/apis/board-api2.js
+++ server/apis/board-api2.js
@@ -1,6 +1,7 @@
 const morgan = require("morgan");
 const url = require("url");
 const uuidAPIkey = require("uuid-apikey");
+const cors = require('cors');
 
 const express = require("express");
 const app = express();
@@ -10,6 +11,7 @@ app.set("port", process.env.PORT || 8080);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors()); // 모든 라우터에 cors 적용
 
 const key = {
     apiKey: "1EVRN2R-31W4XPY-HCFJR1T-6ZZDSBD",
