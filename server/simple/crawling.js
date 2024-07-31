afdf24bd482cbfe82564150588125ca4b7786315
조영상 <51320595+0ssang@users.noreply.github.com>
2024-07-31 19:05:39 +0900

    웹 크롤링 테스트

--- server/simple/crawling.js
+++ server/simple/crawling.js
@@ -13,3 +13,32 @@
  *  each(): 콜백 함수를 파라미터로 받아 태그들이 담긴 배열을 순회하면서 콜백 함수를 실행한다.
  *  find(): html selector를 문자열로 받아 해당 태그를 반환한다.
  */
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getHtml = async () => {
+    try{
+        return await axios.get("https://roadbook.co.kr/category/%EC%8B%A0%EA%B0%84%EC%86%8C%EA%B0%9C");
+    } catch(error){
+        console.error(error);
+    }
+};
+
+getHtml()
+    .then(html => {
+        let ulList = [];
+        const $ = cheerio.load(html.data);
+        const $bodyList = $("div#searchList ol").children("li");
+
+        $bodyList.each(function (i, elem){
+            ulList[i] = {
+                bookList: $(this).find('a').text(),
+                url: $(this).find('a').attr('href'),
+            };
+        });
+
+        const data = ulList.filter(n => n.bookList );
+        return data;
+    })
+    .then(res => console.log(res));
